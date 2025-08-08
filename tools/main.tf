2cf35150417071fbcf2a0a76aa4f9b4d65b72241
chaithanya <chaithanyack656@gmail.com>
2025-08-08 21:46:56 +0530

    learn terraform

--- tools/main.tf
+++ tools/main.tf
@@ -2,7 +2,7 @@ module "tools" {
   for_each = var.tools
   source = "../modules/ec2"
   ami    = var.ami
-  instance_type = each.value["instance_tpye"]
+  instance_type = each.value["instance_type"]
   name = each.key
   zone_id = var.zone_id
 }
