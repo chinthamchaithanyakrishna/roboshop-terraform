70dbc528a81c95bb4a58bc138ad6d47afb136064
chaithanya <chaithanyack656@gmail.com>
2025-08-20 00:58:26 +0530

    learn terraform

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -39,7 +39,6 @@ resource "null_resource" "ansible" {
     }
 
     inline = [
-      "sudo pip3.11 install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/chinthamchaithanyakrishna/roboshop-ansible roboshop.yml -e role_name=${var.name} -e token=${var.token} -e env=${var.env}"
     ]
   }
